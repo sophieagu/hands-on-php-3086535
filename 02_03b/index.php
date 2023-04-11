f5827675a670d0135580d1e0df992ea0935999e5
sophie agudelo <sophieagu12@knights.ucf.edu>
2023-04-11 02:24:59 +0000

    2.3

--- 02_03b/index.php
+++ 02_03b/index.php
@@ -5,5 +5,19 @@ function print_array($a) {
 	echo '</pre>';
 }
 
+function compare_files($a, $b) {
+	$a = pathinfo( $a );
+	$b = pathinfo( $b );
+
+	if ( strcmp( $a['extension'], $b['extension']) == 0 ) {
+		return strcmp( $a['filename'], $b['filename']);
+	}
+
+	return strcmp($a['extention'], $b['extension']);
+}
+
 $dir = 'files';
 $files = scandir($dir);
+
+usort( $files, 'compare_files' );
+print_array( $files );
