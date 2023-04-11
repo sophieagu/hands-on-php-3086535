facfc2ffb55f169f6d2ca9b6176d519ba43e4b7e
sophie agudelo <sophieagu12@knights.ucf.edu>
2023-04-11 03:09:42 +0000

    3.2

--- 03_02b/index.php
+++ 03_02b/index.php
@@ -1,60 +1,86 @@
 <?php
-  function print_array( $output ) {
-    echo '<pre>';
-    print_r( $output );
-    echo '</pre>';
-  }
+function print_array($output)
+{
+	echo '<pre>';
+	print_r($output);
+	echo '</pre>';
+}
+
+function print_img($img)
+{
+	$format = '<figure>
+	  	<img src="%1$s" alt="%2$s">
+		<figcaption>%3$s</figcaption>
+		</figure>';
+
+	printf(
+		$format,
+		$img->url_full_size,
+		$img->description,
+		$img->title
+	);
+}
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Retrieving information from an API</title>
-        <meta name="author" value="Joe Casabona" />
-    </head>
-    <body>
-		<main>
-  			<h1>Recent Images</h1>
-		</main>
-		<style>
-			body {
-				background: #edf2f8;
-				font-family: 'Inter', sans-serf;
-				font-size: 18px;
-			}
 
-			main {
-				background: #FFFFFF;
-				padding: 40px;
-				margin: 0 auto;
-				width: 800px;
-			}
+<head>
+	<title>Retrieving information from an API</title>
+	<meta name="author" value="Joe Casabona" />
+</head>
 
-			figure {
-				background: #333333;
-				padding: 10px;
-				margin: 15px auto;
-				max-width: 95%;
-			}
+<body>
+	<main>
+		<h1>Recent Images</h1>
+		<?php
+		$url = 'https://pixelford.com/api2/image?qty=3';
+		$images = array_slice(json_decode(file_get_contents($url)), 0, 15);
+		foreach ($images as $img) {
+			print_img($img);
+		}
+		?>
+	</main>
+	<style>
+		body {
+			background: #edf2f8;
+			font-family: 'Inter', sans-serf;
+			font-size: 18px;
+		}
 
-			figure img {
-				max-width: 100%;
-				height: auto;
-			}
+		main {
+			background: #FFFFFF;
+			padding: 40px;
+			margin: 0 auto;
+			width: 800px;
+		}
 
-			figcaption {
-				color: #FFFFFF;
-				text-align: center;
-			}
+		figure {
+			background: #333333;
+			padding: 10px;
+			margin: 15px auto;
+			max-width: 95%;
+		}
 
-			form {
-				margin: 25px;
-			}
+		figure img {
+			max-width: 100%;
+			height: auto;
+		}
 
-			form input {
-				width: 100%;
-				padding: 5px;
-				font-size: 20px;
-			}
-		</style>
-    </body>
+		figcaption {
+			color: #FFFFFF;
+			text-align: center;
+		}
+
+		form {
+			margin: 25px;
+		}
+
+		form input {
+			width: 100%;
+			padding: 5px;
+			font-size: 20px;
+		}
+	</style>
+</body>
+
 </html>
