b6a901506131e4e75ce5b9d3b6921e5e91d511ff
sophie agudelo <sophieagu12@knights.ucf.edu>
2023-04-11 03:15:26 +0000

    3.3

--- 03_03b/index.php
+++ 03_03b/index.php
@@ -13,11 +13,61 @@ The table we’re working with is called “speakers”
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Display Speaker Information</title>
-        <meta name="author" value="Joe Casabona" />
-    </head>
-    <body>
-        <!--Content goes here-->
-    </body>
+
+<head>
+    <title>Display Speaker Information</title>
+    <meta name="author" value="Joe Casabona" />
+</head>
+
+<body>
+    <main>
+        <h1>Speakers and Sessions</h1>
+        <table>
+            <thead>
+                <td>Name</td>
+                <td>Bio</td>
+                <td>Session Title</td>
+                <td>Session Desc</td>
+                <td>Track</td>
+            </thead>
+            <tbody>
+                <?php
+                $host = '127.0.01';
+                $user = 'mariadb';
+                $pass = 'mariadb';
+                $db = 'mariadb';
+
+                $mysqli = new mysqli(
+                    $host,
+                    $user,
+                    $pass,
+                    $db
+                );
+
+                $results = $mysqli->query('SELECT * FROM speakers;');
+
+                $format = '<tr>
+					<td>%1$s</td>
+					<td>%2$s</td>
+					<td>%3$s</td>
+					<td>%4$s</td>
+					<td class="label">%5$s</td>
+				</tr>';
+                foreach ($results as $row) {
+
+                    printf(
+                        $format,
+                        $row['name'],
+                        $row['bio'],
+                        $row['session_name'],
+                        htmlentities($row['session_desc']),
+                        $row['track']
+                    );
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+
 </html>
