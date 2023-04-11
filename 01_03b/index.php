781dbc8cc40d5e0c23d5f0faa768b36a3e9c70c3
sophie agudelo <sophieagu12@knights.ucf.edu>
2023-04-11 01:20:17 +0000

    1.3

--- 01_03b/index.php
+++ 01_03b/index.php
@@ -17,8 +17,13 @@ function flip() {
 	return ( 0 == rand(0,1) ) ? 'H' : 'T';
 }
 
-?>
+$H = 0;
+$T = 0;
 
-<div class="coin">
-	<?php echo flip(); ?>
-</div>
+for( $i = 0; $i < 1000; $i++) {
+	$flip = flip();
+	${$flip}++;
+}
+
+echo '<p>Heads was flipped.' . $H/10 . '% of the time. Tails was flipped' . $T/10 . '%.';
+
